58f05620b2dcd19744e914e3579c3040c42c1feb
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-15 10:22:02 +0800

    add routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import generalRouter from './app/routes/general.routes.js';
+import authRouter from './app/routes/auth.routes.js';
 
 const app = express();
 const port = 8080;
@@ -8,8 +9,9 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(authRouter);
 app.use(generalRouter);
 
 app.listen(port, () => {
-  console.log(` App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });

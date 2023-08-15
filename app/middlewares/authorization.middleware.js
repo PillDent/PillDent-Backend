57b22c23147d35382a02216966cd19c4497ed6d3
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-15 10:21:36 +0800

    add auth middleware

--- app/middlewares/authorization.middleware.js
+++ app/middlewares/authorization.middleware.js
@@ -1,23 +1,21 @@
-// import db from '../config/firebase.config.js';
-// import { badResponse } from '../utils/response.js';
+import db from '../config/firebase.config.js';
+import { badResponse } from '../utils/response.js';
 
-// // Check UID
-// const checkUID = async (collection, id, uid) => {
-//   const snapshot = await db.collection(`${collection}`).doc(id).get();
+// Check UID
+const checkUID = async (collection, id, uid) => {
+  const snapshot = await db.collection(`${collection}`).doc(id).get();
 
-//   const snapshotData = snapshot.data();
+  const snapshotData = snapshot.data();
 
-//   if (snapshotData.<collection> !== uid) {
-//     return {
-//       errorUID: true,
-//       statusUID: 403,
-//       checkResponseUID: badResponse(403, 'Not allowed'),
-//     };
-//   }
-//   return {
-//     errorUID: false,
-//     snapshotData,
-//   };
-// };
-
-// export { checkUID };
+  if (snapshotData.renter_id !== uid) {
+    return {
+      errorUID: true,
+      statusUID: 403,
+      checkResponseUID: badResponse(403, 'Not allowed'),
+    };
+  }
+  return {
+    errorUID: false,
+    snapshotData,
+  };
+};

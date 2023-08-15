4bf19b66781a6961712855a6c38d4201cd4a1a66
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-08-15 10:21:56 +0800

    import utils

--- app/controllers/general.controller.js
+++ app/controllers/general.controller.js
@@ -1,5 +1,5 @@
-// import { badResponse, successResponse } from '../utils/response.js';
-// import db from '../config/firebase.config.js';
+import { badResponse, successResponse } from '../utils/response.js';
+import db from '../config/firebase.config.js';
 
 // eslint-disable-next-line consistent-return
 const testApp = async (req, res) => {
